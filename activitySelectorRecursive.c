eaa6c3c953f3dfca90eb8ebb46657e86e9956095
DiegoCorrea <diegocorrea.cc@gmail.com>
2016-05-10 15:12:34 -0300

    finish recursive activity selector

--- activitySelectorRecursive.c
+++ activitySelectorRecursive.c
@@ -1,16 +1,20 @@
 # include <stdio.h>
 # define MAX 100
+# define start 0
+# define finish 1
 
 int activity[2][MAX], n, result[MAX];
 
 void input();
 void output();
 
+void recursiveActivitySelector(int,int);
+
 int main(){
 
   input();
 
-  
+  recursiveActivitySelector(0,n+1);
 
   output();
 
@@ -21,36 +25,42 @@ int main(){
 void input(){
   int i;
 
-  activity[0][0] = 0;
-  activity[1][0] = 0;
+  activity[start][0] = 0;
+  activity[finish][0] = 0;
   result[0] = 0;
 
-  printf("Length: ");
   scanf("%d", &n);
-  printf("%d", n);
 
-  printf("\nStart: ");
-  printf("%d ", activity[0][0]);
   for(i = 1; i <= n ;i++){
-    scanf("%d ", &activity[0][i]);
-    printf("%d ", activity[0][i]);
+    scanf("%d ", &activity[start][i]);
   }
 
-  printf("\nFinish: ");
-  printf("%d ", activity[1][0]);
   for(i = 1; i <= n ;i++){
-    scanf("%d ", &activity[1][i]);
-    printf("%d ", activity[1][i]);
+    scanf("%d ", &activity[finish][i]);
     result[i] = 0;
   }
 }
 void output(){
   int i;
 
-  printf("\nOutput");
-
-  printf("\nActivity Selected: ");
+  printf("\nOutput: ");
   for(i = 0; i <= n ;i++){
-    printf("%d ", result[i]);
+    if(result[i] == 1){
+      printf("\nActivity %d: Start: %d Finish %d | Selected", i, activity[start][i], activity[finish][i]);
+    } else{
+      printf("\nActivity %d: Start: %d Finish %d | Not Selected", i, activity[start][i], activity[finish][i]);
+    }
   }
+}
+
+void recursiveActivitySelector(int pivot, int end){
+  int m = pivot + 1;
+
+  while((m < end) && (activity[start][m] < activity[finish][pivot]))
+    m++;
+
+  if(m < end){
+    recursiveActivitySelector(m,end);
+    result[m] = 1;
+  }else return;
 }
